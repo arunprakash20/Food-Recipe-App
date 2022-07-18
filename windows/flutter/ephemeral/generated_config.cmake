afb8822dfc2b77e0150364fc72a588a2c6dd8b72
arunprakash20 <81683061+arunprakash20@users.noreply.github.com>
2022-07-18 15:18:00 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app"
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app"
+  "FLUTTER_TARGET=C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\aptha\\AndroidStudioProjects\\recipe_suggestion_app\\.dart_tool\\package_config.json"
+)
